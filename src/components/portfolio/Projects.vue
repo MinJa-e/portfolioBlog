8cf11cdbda34d8e06672a39b44425a814df5a442
codeInJ <80948609+codeInJ@users.noreply.github.com>
2021-12-28 16:28:26 +0900

    프로젝트 페이지 디테일 수정

--- src/components/portfolio/Projects.vue
+++ src/components/portfolio/Projects.vue
@@ -1,4 +1,5 @@
 <template>
+<!--나중에 프로젝트 이미지 및 설명은 데이터바인딩으로 넣어서 함수적으로 노출되게 하기-->
     <div class="text-center w-100 padding-section" id="Projects">
       <h1 class="mp-title" style="font-family: 'Black Han Sans','Noto Sans KR', sans-serif;">
         Projects
@@ -8,12 +9,67 @@
     <b-tab title="All" active>
       <b-row>
         <!--왼-->
-        <b-col>
-          왼
+        <b-col cols="5">
+           <img src="@/assets/projectImage/a.jpg" class="projectImage"/>
         </b-col>
         <!--오른-->
         <b-col>
-          오른
+          <b-row>
+            <b-col cols="2">
+          <b>소개</b>
+            </b-col>
+            <b-col>
+              <p>내가 생각하는 나의 성격 검사하기, 친구에게 나의 성격 평가 부탁하기, 나의 성격에 대한 나와 친구의 검사 결과 비교하기</p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="2">
+          <b>사용기술</b>
+            </b-col>
+            <b-col>
+              <p>Vue, Spring</p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="2">
+          <b>담당업무</b>
+            </b-col>
+            <b-col>
+              <p> RESTful API, Axios</p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="2">
+          <b>개발기간</b>
+            </b-col>
+            <b-col>
+              <p>2021.12.10~2021.12.11 ( 1일 소요 )</p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="2">
+          <b>개발인원</b>
+            </b-col>
+            <b-col>
+              <p>1인</p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="2">
+          <b>깃허브</b>
+            </b-col>
+            <b-col>
+              <p>https://github.com/codeInJ</p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col cols="2">
+          <b>주소</b>
+            </b-col>
+            <b-col>
+              <p>http://leeniesta.cafe24.com/</p>
+            </b-col>
+          </b-row>
         </b-col>
       </b-row>
     </b-tab>
@@ -32,6 +88,9 @@ export default {
 </script>
 
 <style>
+.projectImage{
+  width: 100%;
+}
 #projectsTab > div > ul{
   width: 50%;
   margin: auto;
